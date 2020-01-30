68dc265b37cfdcdd5d3f7a82fbaa5ccf220789a7
BrendaVv <brendavanvugt@outlook.com>
2020-01-30 19:37:30 +0100

    UI test

---
+++ Assets/scripts for UI/DisableObject.cs
@@ -0,0 +1,28 @@
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ using System.Collections;
+
+public class DisableObject : MonoBehaviour
+{  
+   // public bool ScriptOn;
+    void Start ()
+    {
+        this.gameObject.GetComponent<Image>().enabled = false;
+    }
+	
+	void Update () 
+	{
+		if (this.enabled == true ) 
+		{
+          
+	      this.enabled = false;
+		  this.gameObject.GetComponent<Image>().enabled = true;
+		}
+		
+		else
+	    {
+			this.gameObject.GetComponent<Image>().enabled = false;
+		}
+		
+	}
+}
---
+++ Assets/scripts for UI/SliderValueToText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderValueToText : MonoBehaviour {
+  public Slider sliderUI;
+  private Text textSliderValue;
+
+  void Start (){
+    textSliderValue = GetComponent<Text>();
+    ShowSliderValue();
+  }
+
+  public void ShowSliderValue () {
+    string sliderMessage = "" + sliderUI.value;
+    textSliderValue.text = sliderMessage;
+  }
+}
