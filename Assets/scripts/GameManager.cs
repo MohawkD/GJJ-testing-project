45b97b54f966669e934721adefdd37f4bdcf4e51
Shao Wenbin <shaowenbin.salih@gmail.com>
2020-02-01 14:22:05 +0100

    smooth move

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -50,6 +50,6 @@ public class GameManager : MonoBehaviour
 //        m_player.gameObject.transform.position = m_hexGridMap.GetGridPosition(m_player.currentPosition);
 //        m_player_2.gameObject.transform.position = m_hexGridMap.GetGridPosition(m_player_2.currentPosition);
         m_player.UpdatePosition(m_hexGridMap.GetGridPosition(m_player.currentPosition));
-        m_player_2.UpdatePosition(m_hexGridMap.GetGridPosition(m_player_2.currentPosition));
+//        m_player_2.UpdatePosition(m_hexGridMap.GetGridPosition(m_player_2.currentPosition));
     }
 }
