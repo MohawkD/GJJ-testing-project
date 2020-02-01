d57910be23e8a25bc46539fc61c8285d444bf231
Shao Wenbin <shaowenbin.salih@gmail.com>
2020-02-01 10:22:47 +0100

    add Wire, GameManager

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)  
+        {
+            Destroy(gameObject);
+        }
+        
+        DontDestroyOnLoad(gameObject);
+        InitGame();
+    }
+
+    private void InitGame()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/Wire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(LineRenderer))]
+public class Wire : MonoBehaviour
+{
+    private LineRenderer m_lineRenderer;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        m_lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Start()
+    {
+        m_lineRenderer.SetPosition(0, this.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var positionCount = m_lineRenderer.positionCount;
+        positionCount++;
+        m_lineRenderer.positionCount = positionCount;
+        m_lineRenderer.SetPosition(positionCount-1, this.transform.position);
+    }
+}
