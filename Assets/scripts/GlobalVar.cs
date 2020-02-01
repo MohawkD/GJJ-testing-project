68e6177c3beab905d8997efc872df62c2dce1882
Shao Wenbin <shaowenbin.salih@gmail.com>
2020-02-01 16:38:13 +0100

    add globalvar

---
+++ Assets/scripts/GlobalVar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalVar
+{
+    //animator parameters
+    public static string startMoving = "startMoving";
+    public static string finishMoving = "finishMoving";
+    public static string startWinningGame = "startWinningGame";
+    public static string startLosingGame = "startLosingGame";
+    public static string endGame = "endGame";
+    public static string startBouncing = "startBouncing";
+    public static string finishBouncing = "finishBouncing";
+    public static string startPowerUp = "startPowerUp";
+    public static string finishPowerUp = "finishPowerUp";
+
+}
