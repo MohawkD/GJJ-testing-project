cf0a554ac929887e584ee998a53b9bbb9234b4b4
Shao Wenbin <shaowenbin.salih@gmail.com>
2020-02-02 14:47:38 +0100

    Audio

---
+++ Assets/scripts/Audio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+}
