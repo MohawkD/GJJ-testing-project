5c1c31c2a2d128408f9ae55f1d27a7310195d770
Shao Wenbin <shaowenbin.salih@gmail.com>
2020-02-02 13:10:48 +0100

    finish scene

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -162,6 +162,9 @@ public class Player : MonoBehaviour
             StopCoroutine(Move());
             //TODO: winning animation
             Debug.Log("Player " + playerNumber + " wins!");
+            
+            GameManager.instance.GameFinish();
+            
             return true;
         }
         return false;
